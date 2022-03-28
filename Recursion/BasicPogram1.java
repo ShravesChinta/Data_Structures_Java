f271e09127d94c37951dcbb95198a56a6270ed7b
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2022-03-27 18:14:48 -0600

    Create BasicPogram1.java

---
+++ Recursion/BasicPogram1.java
@@ -0,0 +1,45 @@
+/** Program to print numbers from 1 to N using recursion **/
+
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s =new Scanner(System.in);
+		int a =s.nextInt();
+		printNTimes(a);
+		
+	}
+	
+	public static void printNTimes(int n ){
+	    if(n==0)
+	    return ;
+	    printNTimes(n-1);
+	    System.out.println(n);
+	}
+}
+
+/***
+
+let us say N=4,
+
+then the output should be 
+1
+2
+3
+4
+
+The stack will look like this 
+
+
+| f(1)  |
+_________
+| f(2)  |
+_________
+|  f(3) |
+_________
+| f(4)  |
+__________
+
+**/
