740e68c4326f48114962e445d1368c77a33016f8
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2022-04-07 19:18:18 -0600

    Create Find_the_Frequency.java

---
+++ Arrays/Find_the_Frequency.java
@@ -0,0 +1,33 @@
+//Geeks for geeks program to find out the frequency of the program
+
+
+/***
+
+Given a vector of N positive integers and an integer X. The task is to find the frequency of X in vector.
+
+Example 1:
+
+Input:
+N = 5
+vector = {1, 1, 1, 1, 1}
+X = 1
+Output: 
+5
+Explanation: Frequency of 1 is 5.
+Your Task:
+Your task is to complete the function findFrequency() which should count the frequency of X and return it.
+
+***/
+
+class Solution{
+    int findFrequency(int A[], int x){
+        int n=A.length;
+        int count=0;
+        for(int i =0;i<n;i++)
+        {
+            if(A[i]==x)
+            count++;
+        }
+        return count;
+    }
+}
