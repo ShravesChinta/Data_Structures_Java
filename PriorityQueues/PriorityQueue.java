361d383b5f7fa28ff308c713294b6a2307de663b
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2022-04-20 17:12:27 -0600

    Implementation of Priority Class in java

---
+++ PriorityQueues/PriorityQueue.java
@@ -0,0 +1,39 @@
+//Implementation of Priority Class in java
+
+
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		PriorityQueue<Integer> minHeap=new PriorityQueue<>();
+		PriorityQueue<Integer> maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+		
+		int[] nums={1,10,5,3,4,7,6,9,8};
+		for(int a:nums)
+		{
+		    minHeap.offer(a);
+		    maxHeap.offer(a);
+		}
+		
+		System.out.println("The min Heap is " + minHeap);
+		System.out.println("The max Heap is "+ maxHeap);
+		System.out.println("__________________________");
+		System.out.println("The size of min Heap is " + minHeap.size());
+		System.out.println("The size of max Heap is " + maxHeap.size());
+		System.out.println("__________________________");
+		//peek returns the minimum value without removing it from the queue
+		System.out.println("The minimum value of min Heap is " + minHeap.peek());
+		//peek returns the maximum value without removing it from the queue
+		System.out.println("The maximum value of max Heap is " + maxHeap.peek());
+		//poll returns the minimum value and removes it from the queue
+		System.out.println("The minimum value of min Heap" + minHeap.poll());
+		System.out.println("__________________________");
+		System.out.println("The size of the min Heap is " + minHeap.size());
+	    System.out.println("__________________________");
+	    //poll returns the maximum value and removes it from the queue
+	    System.out.println("The Maximum value in the queue is " + maxHeap.poll());
+	    System.out.println("The size of max Heap is " + maxHeap.size());
+	    System.out.println("__________________________");
+	}
+}
