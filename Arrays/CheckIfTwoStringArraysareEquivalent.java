2867c39a8a6706da58c248e6255c6ecfadd2f32d
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2022-06-05 18:41:24 -0600

    Create CheckIfTwoStringArraysareEquivalent.java
    #leetcode

---
+++ Arrays/CheckIfTwoStringArraysareEquivalent.java
@@ -0,0 +1,45 @@
+/***
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+A string is represented by an array if the array elements concatenated in order forms the string.
+
+ 
+Example 1:
+
+Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+Output: true
+Explanation:
+word1 represents string "ab" + "c" -> "abc"
+word2 represents string "a" + "bc" -> "abc"
+The strings are the same, so return true.
+Example 2:
+
+Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
+Output: false
+Example 3:
+
+Input: word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]
+Output: true
+
+***/
+
+
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String str1=joinString(word1);
+        String str2=joinString(word2);
+        if(str1.equals(str2))
+            return true;
+        return false;
+    }
+    
+    public String joinString(String[] word)
+    {
+        String str="";
+        for(String a: word)
+        {
+            str=str+a;
+        }
+        return str;
+    }
+}
